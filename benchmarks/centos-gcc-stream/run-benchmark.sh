3bd0bdbce4c9918845e273f8a4edaed59aaf071d
cloudsys <cloudsys@kb-m11.openstacklocal>
2018-05-01 19:47:19 -0500

    script added

--- benchmarks/centos-gcc-stream/run-benchmark.sh
+++ benchmarks/centos-gcc-stream/run-benchmark.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 MB=$(free | grep Mem | tr -s ' ' | cut -d ' ' -f 4);
-ARRAYSIZE=$(( $MB * 40 ));
+ARRAYSIZE=$(( $MB * 1000000 ));
 
 gcc -fstrict-aliasing -mcmodel=large -Ofast -mtune=native -march=native -fopenmp /usr/src/stream/stream.c -DSTREAM_ARRAY_SIZE=$ARRAYSIZE -o stream-gcc
 
@@ -9,4 +9,7 @@ SOCKETS=$(lscpu | grep Socket | tr -s ' ' | cut -d ' ' -f2);
 
 export OMP_NUM_THREADS=$(( $CORESPERSOCKET * $SOCKETS ));
 export GOMP_CPU_AFFINITY="0-$(($OMP_NUM_THREADS - 1))"
+for i in `seq 1 500`
+do 
  ./stream-gcc
+done
